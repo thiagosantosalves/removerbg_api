7ee5ac3f6ac456a40a76d19609b7b8c61f173e73
thiago santos <thiagoindie17@gmail.com>
2023-07-31 18:13:51 -0300

    .

--- src/app/RemoverBGController.ts
+++ src/app/RemoverBGController.ts
@@ -54,8 +54,8 @@ class RemoverBGController {
 
         
         const browser: any = await puppeteer.launch({ 
-            //headless: 'new',
-            headless: false,
+            headless: 'new',
+            //headless: false,
            
             args: [
                 '--no-sandbox', 
