3b41e98aed8682bebe2e1bc3dc6cd401e4008491
thiago santos <thiagoindie17@gmail.com>
2023-08-07 21:18:46 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-/*
-necessary to configure which sites can access the api
 app.use(cors); 
-*/
 
 app.use(router);
 app.use('/files', express.static(path.resolve(__dirname, '..', 'download')));
